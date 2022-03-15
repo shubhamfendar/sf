4471a000e20a573d7aa021c9e0b436b6dc7913d1
shubhamfendar <101207804+shubhamfendar@users.noreply.github.com>
2022-03-15 12:12:04 +0530

    java file

---
+++ Hello.java
@@ -0,0 +1,19 @@
+public class Hello{
+	public static void main(String args[]){
+		System.out.println("Hello World ");
+		C2.sayHello();
+	}
+}
+
+class C2{
+	static void sayHello(){
+		System.out.println("sdfsfsd");
+	}
+}
+
+class C3{
+	public static void main(String args[]){
+		System.out.println("Hello World ");
+		C2.sayHello();
+	}
+}
