14b4a0427dc168e3343933f26f3ae0c3451ebb09
shubhamfendar <101207804+shubhamfendar@users.noreply.github.com>
2022-03-15 12:57:20 +0530

    Add files via upload

---
+++ CarTest2.java
@@ -0,0 +1,20 @@
+public class CarTest2{
+	public static void main(String args[]){
+		Car.noOfTyres = 4;
+		Car.start();
+		
+		Car myCar = new Car();
+		myCar.model="Baleno";
+		myCar.move();
+		
+		//Car c1 = new Car();		
+		//Car c2 = new Car();
+		
+		//c2 = c1;
+		
+		//Car c3 = new Car();
+		//c3 = c2;
+		
+	}
+
+}
